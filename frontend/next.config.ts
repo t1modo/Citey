689c72e13f2391ee7cded0cbbc64a73d09006df3
Timothy Do <tim.do.info@gmail.com>
2026-03-15 11:30:10 -0700

    Attempted to fix deployment issue using Vercel

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,5 +1,11 @@
 import type { NextConfig } from "next";
+import path from "path";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  outputFileTracingRoot: path.resolve(__dirname),
+  turbopack: {
+    root: path.resolve(__dirname),
+  },
+};
 
 export default nextConfig;
